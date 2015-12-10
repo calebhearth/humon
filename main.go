ff9e3fb5584c47957222e2758bce6e49842a9c71
Caleb Thompson <caleb@calebthompson.io>
2015-12-10 14:16:16 -0600

    Database

--- main.go
+++ main.go
@@ -1,13 +1,50 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
-var event = struct {
+func main() {
+	http.HandleFunc("/v1/events/1", EventsShow)
+	log.Fatal(http.ListenAndServe(":4321", nil))
+}
+
+func EventsShow(w http.ResponseWriter, r *http.Request) {
+	event, err := GetEvent(1)
+	if err != nil {
+		sorry(w, err)
+		return
+	}
+
+	chars, err := json.Marshal(event)
+	if err != nil {
+		sorry(w, err)
+		return
+	}
+
+	w.Write(chars)
+}
+
+func sorry(w http.ResponseWriter, err error) {
+	chars, err := json.Marshal(struct {
+		Err string `json:"error"`
+	}{err.Error()})
+	if err != nil {
+		panic(err)
+	}
+
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write(chars)
+}
+
+type Event struct {
 	Address string    `json:"address"`
 	EndedAt time.Time `json:"ended_at"`
 	Id      int64     `json:"id"`
@@ -18,37 +55,38 @@ var event = struct {
 		Id int64 `json:"id"`
 	} `json:"owner"`
 	StartedAt time.Time `json:"started_at"`
-}{
-	"123 Main St",
-	time.Date(2001, 1, 1, 12, 0, 0, 0, time.Local),
-	1,
-	"30.267153",
-	"-97.743061",
-	"Austin",
-	struct {
-		Id int64 `json:"id"`
-	}{1},
-	time.Date(2001, 1, 1, 0, 0, 0, 0, time.Local),
 }
 
-func main() {
-	http.HandleFunc("/v1/events/1", EventsShow)
-	log.Fatal(http.ListenAndServe(":4321", nil))
-}
-
-func EventsShow(w http.ResponseWriter, r *http.Request) {
-	var chars []byte
-	chars, err := json.Marshal(event)
+func GetEvent(id int64) (Event, error) {
+	db, err := sql.Open("postgres", "postgres://localhost/humon_development?sslmode=disable")
 	if err != nil {
-		chars, err := json.Marshal(struct {
-			Err string `json:"error"`
-		}{err.Error()})
-		if err != nil {
-			panic(err)
-		}
+		return Event{}, fmt.Errorf("Error connection: " + err.Error())
+	}
+	defer db.Close()
 
-		w.Write(chars)
+	var (
+		eventId, ownerId                         int64
+		createdAt, updatedAt, endedAt, startedAt time.Time
+		address, name, lat, lon                  string
+	)
+
+	err = db.QueryRow("SELECT * FROM events WHERE id = $1", 1).
+		Scan(&eventId, &createdAt, &updatedAt, &endedAt, &name, &startedAt, &ownerId, &address, &lat, &lon)
+
+	if err != nil {
+		return Event{}, err
 	}
 
-	w.Write(chars)
+	return Event{
+		Address: address,
+		EndedAt: endedAt,
+		Id:      eventId,
+		Lat:     lat,
+		Lon:     lon,
+		Name:    name,
+		Owner: struct {
+			Id int64 `json:"id"`
+		}{ownerId},
+		StartedAt: startedAt,
+	}, nil
 }
--- spec/events_api_spec.rb
+++ spec/events_api_spec.rb
@@ -1,6 +1,7 @@
 require "spec_helper"
 require "net/http"
 require "json"
+require "date"
 
 describe "GET /v1/events/:id" do
   it "returns an event by :id" do
@@ -8,18 +9,18 @@ describe "GET /v1/events/:id" do
 
     expect(response).to eq(
       "address" => "123 Main St",
-      "ended_at" => "12:00 AM 1/1/2001",
+      "ended_at" => "2001-01-01T00:00:00Z",
       "id" => 1,
       "lat" => "30.267153",
       "lon" => "-97.743061",
       "name" => "Austin",
       "owner" => { "id" => 1 },
-      "started_at" => "1/1/2001",
+      "started_at" => "2001-01-01T00:00:00Z",
     )
   end
 
   def get(path)
     response = Net::HTTP.get(URI("http://localhost:4321" + path))
-    JSON.parse(response.body)
+    JSON.parse(response)
   end
 end
